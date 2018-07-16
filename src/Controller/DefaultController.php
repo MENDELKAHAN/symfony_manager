0c73dc4947ef87f62e1297413942630704cce387
Mendel Kahan <mendy@famkahan.com>
2018-07-17 00:35:23 +0100

    added twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,15 +1,16 @@
 <?php
 // src/Controller/DefaultController.php
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-class DefaultController
-{
+class DefaultController extends AbstractController{
 /**
 * @Route("/hello/{name}")
 */
 public function index($name) {
-	return new Response("Hello $name!");
+	return $this->render('default/index.html.twig', [
+'name' => $name,
+ ]);
 }
 }
 
