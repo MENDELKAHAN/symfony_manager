e2fe8a601a2b5c67aa5a2037978a3052d887f5e6
Mendel Kahan <mendy@famkahan.com>
2018-07-17 02:12:05 +0100

    added logger

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,16 +1,29 @@
 <?php
 // src/Controller/DefaultController.php
 namespace App\Controller;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController{
 /**
 * @Route("/hello/{name}")
 */
-public function index($name) {
+public function index($name, LoggerInterface $logger){
+	$logger->info("Saying hello to $name!");
 	return $this->render('default/index.html.twig', [
 'name' => $name,
  ]);
+}
+
+/**
+* @Route("/api/hello/{name}")
+*/
+public function apiExample($name)
+{
+return $this->json([
+'name' => $name,
+'symfony' => 'rocks',
+]);
 }
 }
 
